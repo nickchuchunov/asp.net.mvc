3c49ed585fe2b40af6c5f1e67b1d4808ec9223b6
nickchuchunov <nick.chuchunov@yandex.ru>
2021-11-26 19:51:45 +0300

    создание 1 практического задания

---
+++ asp.net.mvc_lesson1/asp.net.mvc_lesson1/asp.net.mvc_lesson1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asp.net.mvc_lesson1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+        }
+    }
+}
